038f648ffd61bc37accb182b7ce0e239d6efa924
belemaaa <graceitamunoala@gmail.com>
2024-11-09 07:42:19 +0100

    fixed type errors in +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
+// @ts-nocheck
+
     import "../app.css";
     import Footer from "../components/Footer.svelte";
     import Header from "../components/Header.svelte";
 
-    let y = 0;
+    let y;
     let innerHeight = 0;
     let innerWidth = 0;
 
@@ -14,13 +16,17 @@
 
 <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
     <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10]" + (
-        y > 0 ? 'opacity-full pointer-events-auto' : 'pointer-events-none opacity-0'
+        y > 0 
+            ? 'opacity-full pointer-events-auto' 
+            : 'pointer-events-none opacity-0'
     )}>
-        <button>
-            <i class="fa-solid fa-arrow-up"></i>
+        <!-- svelte-ignore a11y_consider_explicit_label -->
+        <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer">
+            <!-- svelte-ignore element_invalid_self_closing_tag -->
+            <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
         </button>
     </div>
-    <Header />
+    <Header y={y}/>
     <slot />
     <Footer/>
 </div>
