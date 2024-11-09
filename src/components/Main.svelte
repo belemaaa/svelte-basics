53d5a20d5820efe8f2e5f20abb3276f1823e92c7
belemaaa <graceitamunoala@gmail.com>
2024-11-09 09:19:23 +0100

    created header and section1 in main

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,34 @@
-james
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-0 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="text-violet-400">Grace</span> Itamunoala <br/>Full Stack <span class="text-violet-400">Developer</span>
+            </h2>
+
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">Stack</span> includes Javascript (React),
+                Typescript (SvelteKit or Angular), TailwindCSS, Node.js (Typescript) + Express.js (Typescript), Python (Django) & C# (.NET)
+            </p>
+
+            <a  
+                href="#" 
+                target="_blank"
+                class="blueShadow mx-auto lg:mr-auto text-base sm:text-large md:text-xl relative overflow-hidden px-6 py-3 group rounded-full 
+                bg-white text-slate-950 cursor-pointer"
+            >
+                <div 
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                />
+                <h4 class="relative z-[9]">Get in touch &rarr;</h4>
+            </a>
+        </div>
+
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src="" alt="profile image" class="object-cover z-[2] max-h-[70vh]"/>
+        </div>
+    </section>
+</main>
